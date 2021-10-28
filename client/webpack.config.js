4db19185d92218d0d315ea17ee102882b72002de
Lance Turbes Jr <waywardpooch@users.noreply.github.com>
2021-10-28 16:38:09 -0700

    separated babel config from webpack config

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -19,12 +19,7 @@ module.exports = {
       {
         test: /\.?js$/,
         exclude: /node_modules/,
-        use: {
-          loader: "babel-loader",
-          options: {
-            presets: ["@babel/preset-env", "@babel/preset-react"],
-          },
-        },
+        use: { loader: "babel-loader" },
       },
       {
         test: /\.css$/i,
