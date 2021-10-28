d3329aea2d74bcc91c04a1548a13a85a7f2b3a98
Lance Turbes Jr <waywardpooch@users.noreply.github.com>
2021-10-28 13:50:23 -0700

    added [GET] /api with default response message

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 require("dotenv").config();
 
 // Import dependencies
-// const path = require("path");
+const path = require("path");
 const express = require("express");
 
 // Declare server
@@ -10,6 +10,20 @@ const server = express();
 
 // Use middleware
 server.use(express.json());
+
+// Handle requests
+server.get("/api", (req, res) => {
+  res.status(200).json({
+    message: `
+    You've reached the API! 
+    There's no real endpoints right now, though.
+    Check again some other day, and I might add a few!
+  `,
+  });
+});
+server.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "index.html"));
+});
 
 // Listen for requests
 const PORT = process.env.PORT;
