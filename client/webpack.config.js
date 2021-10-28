37bb3cc6bdd390b86baedec35ed0457743ac795d
Lance Turbes Jr <waywardpooch@users.noreply.github.com>
2021-10-28 15:48:41 -0700

    fixed client not rendering

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,17 +1,11 @@
 const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  entry: path.resolve(__dirname, "./src/index.js"),
+  entry: "./src/index.js",
   output: {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
   },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, "./dist/index.html"),
-    }),
-  ],
   module: {
     rules: [
       {
