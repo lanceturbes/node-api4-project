cc792e7dd3e4625dd3729382342b276966ae548d
Lance Turbes Jr <waywardpooch@users.noreply.github.com>
2021-10-28 16:30:00 -0700

    separated build and public directories

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   entry: "./src/index.js",
@@ -6,6 +7,13 @@ module.exports = {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Lance's Spot",
+      filename: "index.html",
+      template: "public/index.html",
+    }),
+  ],
   module: {
     rules: [
       {
