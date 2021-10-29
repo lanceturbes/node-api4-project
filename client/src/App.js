c3b73165fafd09858d37413853922bf31ca475a5
Lance Turbes Jr <waywardpooch@users.noreply.github.com>
2021-10-28 20:41:59 -0700

    added styles and endpoint list on home page

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,17 @@ import React from "react";
 const App = () => {
   return (
     <div className="app">
-      <h1>It works!</h1>
+      <h1>Lance's Spot</h1>
+      <h3>Welcome! This is my first full-stack web application.</h3>
+      <h3>You can find my API endpoints listed below:</h3>
+      <p>
+        Note: These endpoints are currently "dumb" and data is not persistent.
+      </p>
+      <p>[GET] /api/users -- returns an array of currently registered users</p>
+      <p>
+        [POST] /api/login -- returns a welcome message for the user logging in
+      </p>
+      <p>[POST] /api/register -- returns the newly registered user</p>
     </div>
   );
 };
