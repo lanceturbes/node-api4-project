dba2f935dd0fd62e631372ce915280e4f55ecea7
Lance Turbes Jr <waywardpooch@users.noreply.github.com>
2021-10-28 20:23:12 -0700

    removed .env import for deployment

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 // Use environment varibles from ".env"
-require("dotenv").config();
+// require("dotenv").config();
 
 const app = require("./api/server");
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`
     Link to site: http://localhost:${PORT}
